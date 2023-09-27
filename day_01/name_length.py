f01e3f4b1c37ae00c149314ab7eb950913cacb82
Barney <b.wilton1993@gmail.com>
2023-09-27 18:00:16 +0100

    created name_length.py that prints out the length of the supplied name using input()

---
+++ day_01/name_length.py
@@ -0,0 +1,6 @@
+# Can be done but is messy with nested functions
+# print(len(input("What is your name? ")))
+
+# This is a much tidier solution and easier to read
+name = input("What is your name? ")
+print(len(name))
