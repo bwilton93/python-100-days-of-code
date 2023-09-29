98d5c788564e6ceb19c18de4ffcd2ea212868725
Barney <b.wilton1993@gmail.com>
2023-09-29 15:06:18 +0100

    Basic treasure island control flow program created

--- day_03/treasure_island.py
+++ day_03/treasure_island.py
@@ -20,5 +20,53 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/______/_
 *******************************************************************************'''
 
-print(initial_string)
 
+def game_over():
+    print("Game Over!")
+    exit()
+
+
+print(initial_string)
+print("Welcome to Treasure Island. Your mission is to find the treasure!")
+
+# * 'You\'re at a crossroad. Where do you want to go? Type "left" or "right"'
+# * 'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.'
+# * "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?"
+# * "It\'s a room full of fire. Game Over."
+# * "You found the treasure! You Win!"
+# * "You enter a room of beasts. Game Over."
+# * "You chose a door that doesn\'t exist. Game Over."
+# * "You get attacked by an angry trout. Game Over."
+# * "You fell into a hole. Game Over."
+
+choice = input(
+    'You\'re at a crossroad. Where do you want to go? Type "left" or "right". '
+).lower()
+
+if choice != "left":
+    print("You fell in a pit and died.")
+    game_over()
+
+choice = input(
+    'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. '
+).lower()
+
+if choice != "wait":
+    print("You got attacked by Trout and died.")
+    game_over()
+
+choice = input(
+    "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? "
+).lower()
+
+if choice == "red":
+    print("It's a room full of fire.")
+    game_over()
+elif choice == "blue":
+    print("You were eaten by beasts.")
+    game_over()
+elif choice == "yellow":
+    print("You found the treasure! You Win!")
+else:
+    print("You chose a door that doesn't exist.")
+    game_over()
