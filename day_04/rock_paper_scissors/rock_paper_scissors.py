ac332062524b321c1bcd5337d94971ada100ec5a
Barney Wilton <B.Wilton1993@gmail.com>
2023-10-06 15:52:51 +0100

    Created control loop to force player to choose a correct input number

--- day_04/rock_paper_scissors/rock_paper_scissors.py
+++ day_04/rock_paper_scissors/rock_paper_scissors.py
@@ -1,7 +1,16 @@
 import random, selection
 
+choice_made = False
+
 print('Welcome to rock, paper, scissors!')
-player_choice = input('What do you choose? Type 1 for rock, 2 for paper or 3 for scissors.\n')
+
+while(not choice_made):
+  player_choice = input('What do you choose? Type 1 for rock, 2 for paper or 3 for scissors.\n')
+  if(player_choice != '0' and player_choice != '1' and player_choice != '2'):
+    print('That is not a valid input, try again.')
+  else:
+    choice_made = True
+
 player_choice = int(player_choice) - 1
 computer_choice = random.randint(0, 2)
 
