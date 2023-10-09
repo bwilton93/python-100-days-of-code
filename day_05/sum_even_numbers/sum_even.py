f7ca33bfa6d1f60d1c26fefa2d7de7a2d1778668
Barney <b.wilton1993@gmail.com>
2023-10-09 20:31:01 +0100

    Finished sum_even.py

---
+++ day_05/sum_even_numbers/sum_even.py
@@ -0,0 +1,9 @@
+target = int(input()) # Enter a number between 0 and 1000
+
+output = 0
+
+# This will sum all even numbers, must start at 0 to correctly use step value of 2
+for i in range(0, target + 1, 2):
+  output += i 
+
+print(output)
