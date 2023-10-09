52ed6d92aecf6088afe6ee899b68a8fd998de42f
Barney <b.wilton1993@gmail.com>
2023-10-09 20:25:24 +0100

    Summed 1 - 100 using a for loop with the range function

--- day_05/highest_score/highest_score.py
+++ day_05/highest_score/highest_score.py
@@ -3,9 +3,10 @@ student_scores = input().split()
 for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 
-# Write your code below this row 👇
+# This stores a temp variable to be compared against using the for loop
 output_score = 0
 
+# On each loop this compares the score to the current output, if the score is greater it updates the output score
 for score in student_scores:
   if(score > output_score):
     output_score = score
