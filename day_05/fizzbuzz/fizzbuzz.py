9bd2da416233863f7b7e4472138cb327c930097d
Barney <b.wilton1993@gmail.com>
2023-10-10 13:08:45 +0100

    Created user input validation for target, it must be a valid integer

--- day_05/fizzbuzz/fizzbuzz.py
+++ day_05/fizzbuzz/fizzbuzz.py
@@ -1,4 +1,15 @@
-for i in range(1, 101):
+num_entered = False
+
+while not num_entered:
+  target = input('Enter a number: ')
+
+  try:
+    target = int(target)
+    num_entered = True
+  except:
+    print('Please enter a valid integer.')
+
+for i in range(1, target + 1):
   if i % 3 == 0 and i % 5 == 0:
     print('FizzBuzz')
   elif i % 3 == 0:
