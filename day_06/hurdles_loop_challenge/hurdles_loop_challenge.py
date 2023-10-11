c0aaeb0397d050cb592485222656620380844af3
Barney <b.wilton1993@gmail.com>
2023-10-11 11:58:36 +0100

    Finished the hurdles loop challenge in day 06

---
+++ day_06/hurdles_loop_challenge/hurdles_loop_challenge.py
@@ -0,0 +1,18 @@
+# Code for https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+def turn_right():
+    for i in range(0, 3):
+        turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+ 
+for i in range(6):
+    move()
+    jump()
